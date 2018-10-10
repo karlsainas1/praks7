f1b37fd8b44a41f677c168919962bff78b47351b
karlsainas1 <karl.sainas@khk.ee>
2018-10-10 12:40:38 +0300

    added hiding feature
    Signed-off-by: karlsainas1 <karl.sainas@khk.ee>

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
 let poundOutput = document.getElementById('poundOutput');
 let ounceOutput = document.getElementById('ounceOutput');
+let output = document.getElementById('output')
 //Listening to events
-
+output.style.visibility= 'hidden';
 kgInput.addEventListener('input', event =>{
+     let kg = event.target.value;   
     
-    let kg = event.target.value;          
+    if (kg == 0 ) {
+         output.style.visibility= 'hidden';
+    }else {
+        output.style.visibility= 'visible';
+        
+    }
+          
+   
     gramOutput.innerHTML =`${kg*1000} g`;           //grams
     poundOutput.innerHTML =`${kg*2.2046} lb `;   //pounds  
     ounceOutput.innerHTML =`${kg*35.274} oz `;  //ounces
