b7a417c9adc40118181513cc8d3e5563405be930
karlsainas1 <karl.sainas@khk.ee>
2018-10-10 12:27:20 +0300

    lisatud naelade ja untside kalkuleerimisvõimalus
    Signed-off-by: karlsainas1 <karl.sainas@khk.ee>

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ let ounceOutput = document.getElementById('ounceOutput');
 
 kgInput.addEventListener('input', event =>{
     
-    let kg = event.target.value;
-    gramOutput.innerHTML =`${kg*1000} g`;
-    poundOutput.innerHTML =`${kg*2.205} pounds `;
+    let kg = event.target.value;          
+    gramOutput.innerHTML =`${kg*1000} g`;           //grams
+    poundOutput.innerHTML =`${kg*2.2046} lb `;   //pounds  
+    ounceOutput.innerHTML =`${kg*35.274} oz `;  //ounces
+
 })
